3831292970f2e5d298610cdbc3f74217afb7f837
Kloetzchengrafik <65603725+Kloetzchengrafik@users.noreply.github.com>
2020-12-28 10:55:22 +0100

    Update test.py

--- primzahlen-algo/test.py
+++ primzahlen-algo/test.py
@@ -1,6 +1,12 @@
+# Funktion erstellen
 def isPrime(zahl):
+    # for Schleife erstellen, von 2 bis zur angegebenen Zahl
     for i in range(2, zahl):
+        # Rest errechnen
         hallo = zahl % i
+        # Überprüfen: Rest vorhanden?
         if hallo == 0:
+            # JA: Abbrechen; Teiler gefunden; Keine Primzahl
             return False
+    # Kein Teiler gefunden: Primzahl
     return True
